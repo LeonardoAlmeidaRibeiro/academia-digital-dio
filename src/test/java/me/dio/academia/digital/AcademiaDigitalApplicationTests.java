141da758a5d923ea76f90f5e4b50fce8fc1521bf
LeonardoAlmeidaRibeiro <leonardo.a.ribeiro.21@gmail.com>
2022-05-15 16:05:47 -0300

    project finalizado

---
+++ src/test/java/me/dio/academia/digital/AcademiaDigitalApplicationTests.java
@@ -0,0 +1,14 @@
+package me.dio.academia.digital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademiaDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+// projeto feito por leonardo.
